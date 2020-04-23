6f2015da1cf934b1da329822d8a30bac1dc3539b
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-04-23 14:51:39 +0900

    add flash_massage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
-  
-  resources :users, only: :show
 
+  root "messages#index"
+  resources :users, only: [:edit, :update]
 end
