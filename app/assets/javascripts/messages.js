333c6ef1e60b013d47c2d7684bd0458f0212a3dd
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-04-28 15:41:11 +0900

    completed automatic-update-functoin

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,8 +1,9 @@
 $(function () {
+
   function buildMessage(message) {
     if (message.image) {
       var html =
-        `<div class="message-list__box">
+        `<div class="message-list__box" data-message-id=${message.id}>
           <div class="message-list__box--top">
             <div class="message-list__box--top--name">
               ${message.user_name}
@@ -23,7 +24,7 @@ $(function () {
       return html;
     } else {
       var html =
-        `<div class="message-list__box">
+        `<div class="message-list__box" data-message-id=${message.id}>
           <div class="message-list__box--top">
             <div class="message-list__box--top--name">
               ${message.user_name}
@@ -42,7 +43,28 @@ $(function () {
     };
   }
 
-
+  var reloadMessages = function () {
+    var last_message_id = $('.message-list__box:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: { id: last_message_id }
+    })
+      .done(function (messages) {
+        if (messages.length !== 0) {
+          var insertHTML = '';
+          $.each(messages, function (i, message) {
+            insertHTML += buildMessage(message)
+          });
+          $('.message-list').append(insertHTML);
+          $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight });
+        }
+      })
+      .fail(function () {
+        alert('error');
+      });
+  };
 
   $('#new_message').on('submit', function (e) {
     e.preventDefault();
@@ -66,5 +88,9 @@ $(function () {
       .fail(function () {
         alert("メッセージ送信に失敗しました");
       })
-  })
+
+  });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
